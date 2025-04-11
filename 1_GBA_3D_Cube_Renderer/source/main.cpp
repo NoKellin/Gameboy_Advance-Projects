7423c7055da7cd98255c797a39bd7acdbcf06d0e
NoKellin <kellinschaap@gmail.com>
2025-04-11 16:58:18 +0200

    Project from C to CPP + Cube Setup

---
+++ 1_GBA_3D_Cube_Renderer/source/main.cpp
@@ -0,0 +1,74 @@
+#include "main.h"
+#include <vector>
+
+typedef struct
+{
+    float x;
+    float y;
+    float z;
+} point;
+
+typedef struct
+{
+    point p[3];
+} triangle;
+
+typedef struct
+{
+    std::vector<triangle> tris;
+} mesh;
+
+int main()
+{
+    REG_DISPCNT = DCNT_MODE4 | DCNT_BG2;
+    setup_vid_base_pal();
+
+    change_bg_clr(WHITE);
+
+    mesh cube;
+    cube.tris = {
+        // SOUTH
+        {0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f},
+
+        // EAST
+        {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f},
+        {1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f},
+
+        // NORTH
+        {1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f},
+        {1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f},
+
+        // WEST
+        {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f},
+        {0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f},
+
+        // TOP
+        {0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f},
+        {0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f},
+
+        // BOTTOM
+        {1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f},
+        {1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f},
+
+    };
+
+    while (1)
+    {
+        // --- WAIT FOR NEW UPDATE ---
+        vid_vsync();
+
+        // --- REMOVE OLD INFO ---
+        clear_screen();
+        scan_keys();
+
+        // --- UPDATE INFO ---
+
+        // --- DRAW NEW INFO ---
+
+        // --- SHOW NEW INFO ---
+        vid_flip();
+    }
+
+    return 0;
+}
